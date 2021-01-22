e0d4df10e3f149d0994b2e4f1ba70ef2c5cc6b3a
keyreal174 <keyreal174@gmail.com>
2021-01-22 05:22:45 -0500

    implement post delete logic

---
+++ src/components/app/Profile/DeletePost.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useRef, Fragment } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+function DeletePost(props) {
+  const handleDeletePost = async () => {
+    await props.deletePost(props.slug);
+    props.closeModal();
+  };
+
+  return (
+    <>
+      <Modal
+        show={props.show}
+        backdrop="static"
+        keyboard={false}
+        size="md"
+        onHide={props.closeModal}
+      >
+        <Modal.Header>
+          <Modal.Title>Delete post</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Please confirm that you want to delete this post and any associated
+          answers and comments.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            className="btn-white"
+            variant="outline-primary"
+            onClick={props.closeModal}
+          >
+            Cancel
+          </Button>
+          <Button
+            className="btn-white modal-primary-button"
+            variant="outline-primary"
+            onClick={handleDeletePost}
+          >
+            Delete Post
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default DeletePost;
