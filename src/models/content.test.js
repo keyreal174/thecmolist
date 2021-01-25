3d6613c652cf3f664fd101845f14bae7bc63832a
keyreal174 <keyreal174@gmail.com>
2021-01-24 23:10:28 -0800

    Remove profile stats model and merge it into profile model Update AddPostModal to use profile model to fetch groups Update api_profilestats mock to return groups

--- src/models/content.test.js
+++ src/models/content.test.js
@@ -5,20 +5,6 @@ const axios = require("axios");
 jest.mock("axios");
 
 describe("contentModel model", () => {
-  it("reducer: setGroups", () => {
-    const store = init({
-      models: { contentModel },
-    });
-    const groups = [{ MOCK_KEY: "MOCK_VALUE" }];
-
-    store.dispatch.contentModel.setGroups({
-      groups: groups,
-    });
-
-    const contentModelData = store.getState().contentModel;
-    expect(contentModelData.content).toEqual({ groups: { groups } });
-  });
-
   it("effect: contentModel shareContent error catch", async () => {
     const store = init({
       models: { contentModel },
@@ -27,24 +13,5 @@ describe("contentModel model", () => {
     const validate = store.dispatch.contentModel.shareContent();
 
     await expect(validate).rejects.toThrow("Could not save content.");
-  });
-
-  it("effect: contentModel getContent", async () => {
-    const store = init({
-      models: { contentModel },
-    });
-    const groups = [{ MOCK_KEY: "MOCK_VALUE" }];
-
-    axios.get.mockResolvedValue({
-      data: {
-        groups,
-      },
-    });
-
-    await store.dispatch.contentModel.getContent();
-
-    const contentModelData = store.getState().contentModel;
-
-    expect(contentModelData.content).toEqual({ groups });
   });
 });
