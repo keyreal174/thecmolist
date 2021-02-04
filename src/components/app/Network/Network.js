c1e8d3fa294f309e8c9cceeca97a6a77faa8f682
keyreal174 <keyreal174@gmail.com>
2021-02-04 18:53:29 -0300

    Implement filters in network

--- src/components/app/Network/Network.js
+++ src/components/app/Network/Network.js
@@ -4,10 +4,67 @@ import { Container, Form } from "react-bootstrap";
 import Header from "../base/Header/Header";
 import Footer from "../base/Footer/Footer";
 import Banner from "../base/Banner/Banner";
+import Filter from "../base/Filter/Filter";
 import InviteModal from "../base/ShareModule/InviteModal";
 import Article from "../base/Article/Article";
+import ActivityIndicator from "../base/ActivityIndicator/ActivityIndicator";
 import Analytics from "../../util/Analytics";
 import "./network.css";
+
+const renderFeed = (props, fetchData) => {
+  const feedData = props.activeFeed;
+
+  return (
+    <>
+      {feedData &&
+        feedData.map((feed, idx) => {
+          let badge = null;
+          const isLocallyConnected = props.localConnectedUsers.includes(
+            feed.username
+          );
+          const isConnected = feed.isConnected || isLocallyConnected;
+
+          if (!feed.disableConnect) {
+            badge = !isConnected ? (
+              <button
+                className="btn connect-button"
+                type="button"
+                onClick={() => {
+                  props.connectUser(feed);
+                }}
+              >
+                Connect
+              </button>
+            ) : (
+              <span className="connected-label">Connected</span>
+            );
+          }
+
+          return (
+            <Article
+              key={idx}
+              className={idx !== 0 ? "mt-1" : ""}
+              {...feed}
+              badge={badge}
+            />
+          );
+        })}
+      {props.moreData && (
+        <div className="row">
+          <div className="col-md-2 mt-2 mx-auto">
+            <button
+              className="btn btn__load-more"
+              type="button"
+              onClick={fetchData}
+            >
+              Show more
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 const Network = (props) => {
   const [sortOrder, setSortOrder] = useState("Top");
@@ -20,8 +77,38 @@ const Network = (props) => {
   useEffect(() => {
     fetchData();
   }, []);
-
-  let feedData = props.feedData;
+  const [filterIdx, setFilterIdx] = useState(0);
+  const [filters, setFilters] = useState([]);
+  const changeNetworkFilter = (filter) => {
+    props.changeNetworkFilter(filter);
+  };
+  useEffect(() => {
+    const getProfileStats = async () => props.getProfileStats();
+    getProfileStats().then((profileStats) => {
+      let newFilters = [
+        { title: "My Network", slug: "my-network", enabled: true },
+        { title: "My Peers", slug: "my-peers", enabled: true },
+        { title: "My Experts", slug: "my-experts", enabled: true },
+      ];
+      if (profileStats && profileStats.profile && profileStats.profile.groups) {
+        newFilters = newFilters.concat(
+          profileStats.profile.groups.map((group) => {
+            return {
+              title: group.name,
+              slug: group.slug,
+              enabled: true,
+            };
+          })
+        );
+      }
+      setFilters(newFilters);
+      changeNetworkFilter(newFilters[filterIdx].slug);
+    });
+  }, []);
+  const changeFilter = (idx) => {
+    setFilterIdx(idx);
+    changeNetworkFilter(filters[idx].slug);
+  };
   return (
     <>
       <Container className="height-100">
@@ -31,6 +118,14 @@ const Network = (props) => {
             title="CMOlist Members"
             img="https://d3k6hg21rt7gsh.cloudfront.net/directory.png"
           />
+          <div>
+            <Filter
+              className="mt-1"
+              filterIdx={filterIdx}
+              filters={filters}
+              onChange={(idx) => changeFilter(idx)}
+            />
+          </div>
           <div className="sort">
             <div className="section-break" />
             <span>Sort by:</span>
@@ -48,50 +143,10 @@ const Network = (props) => {
             </div>
           </div>
 
-          {feedData &&
-            feedData.map((feed, idx) => {
-              let badge = null;
-              const isLocallyConnected = props.localConnectedUsers.includes(
-                feed.username
-              );
-              const isConnected = feed.isConnected || isLocallyConnected;
-              if (!feed.disableConnect) {
-                badge = !isConnected ? (
-                  <button
-                    className="btn connect-button"
-                    type="button"
-                    onClick={() => {
-                      props.connectUser(feed);
-                    }}
-                  >
-                    Connect
-                  </button>
-                ) : (
-                  <span className="connected-label">Connected</span>
-                );
-              }
-              return (
-                <Article
-                  key={idx}
-                  className={idx !== 0 ? "mt-1" : ""}
-                  {...feed}
-                  badge={badge}
-                />
-              );
-            })}
-
-          {props.moreData && (
-            <div className="row">
-              <div className="col-md-2 mt-2 mx-auto">
-                <button
-                  className="btn btn__load-more"
-                  type="button"
-                  onClick={fetchData}
-                >
-                  Show more
-                </button>
-              </div>
-            </div>
+          {props.loadingNetwork ? (
+            <ActivityIndicator />
+          ) : (
+            renderFeed(props, fetchData)
           )}
 
           <InviteModal
@@ -112,9 +167,12 @@ const Network = (props) => {
 
 const mapState = (state) => {
   return {
+    activeFilter: state.networkModel.activeFilter,
+    activeFeed: state.networkModel.activeFeed,
     feedData: state.networkModel.feedData,
     moreData: state.networkModel.moreData,
     localConnectedUsers: state.userModel.localConnectedUsers,
+    loadingNetwork: state.networkModel.loadingNetwork,
   };
 };
 
@@ -123,6 +181,8 @@ const mapDispatch = (dispatch) => {
     fetchNetwork: dispatch.networkModel.fetchNetwork,
     saveUserInvite: dispatch.userModel.saveInvite,
     connectUser: dispatch.userModel.connectUser,
+    getProfileStats: dispatch.profileModel.getProfileStats,
+    changeNetworkFilter: dispatch.networkModel.changeNetworkFilter,
   };
 };
 
--- src/models/network.js
+++ src/models/network.js
@@ -1,23 +1,45 @@
 import axios from "axios";
-const networkRequest = (sort, token) => {
+
+const networkRequest = (sort, filter, token) => {
   let headers = {
     "timezone-offset": new Date().getTimezoneOffset(),
   };
+  let url = `/api/network?sort=${sort.toLowerCase()}`;
+  if (filter) {
+    url += `&filter=${filter}`;
+  }
   if (token) {
     headers.token = token;
   }
-  return axios.get(`/api/network?sort=${sort.toLowerCase()}`, { headers });
+  return axios.get(url, { headers });
 };
 
 export default {
   name: "networkModel",
   state: {
+    activeFeed: "",
+    activeFilter: "",
     feedData: [],
+    loadingNetwork: false,
     moreData: false,
-    token: null,
     sortOrder: "",
+    token: null,
   },
   reducers: {
+    initFeedDataForKey: (oldState, filterKey) => {
+      if (!filterKey) {
+        throw new Error("Expected filterKey");
+      }
+      let newState = { ...oldState };
+      newState.feedData[filterKey] = {
+        title: filterKey,
+        token: "",
+        data: [],
+        moreData: false,
+        enabled: true,
+      };
+      return newState;
+    },
     setData: (oldState, data) => {
       return {
         ...oldState,
@@ -30,6 +52,43 @@ export default {
         sortOrder: data.sortOrder,
       };
     },
+    setLoading: (oldState, data) => {
+      return {
+        ...oldState,
+        loadingNetwork: data,
+      };
+    },
+    setFeedDataForKey: (oldState, filter, data) => {
+      if (!(filter in oldState.feedData)) {
+        throw new Error("No feed data for specified key!");
+      }
+      let newState = {
+        ...oldState,
+        activeFilter: filter,
+      };
+      const feedData = data.feedData;
+      const token = data.token;
+      const currentFeed = oldState.feedData[filter];
+      if (feedData != null && feedData.length > 0) {
+        currentFeed.moreData = true;
+        currentFeed.token = token;
+        currentFeed.data = currentFeed.data.concat(feedData);
+      } else {
+        currentFeed.moreData = false;
+      }
+      newState.activeFeed = currentFeed.data.slice();
+      return newState;
+    },
+    setActiveFilter: (oldState, filterKey) => {
+      if (!(filterKey in oldState.feedData)) {
+        throw new Error("No feed data for specified key!");
+      }
+      return {
+        ...oldState,
+        activeFilter: filterKey,
+        activeFeed: oldState.feedData[filterKey].data,
+      };
+    },
   },
   effects: (dispatch) => ({
     async fetchNetwork(sort, rootState) {
@@ -37,10 +96,37 @@ export default {
         rootState.networkModel.sortOrder === sort
           ? rootState.networkModel.token
           : null; // reset token if sort order changed
-      const response = await networkRequest(sort, token);
+      let filter = rootState.networkModel.activeFilter;
+      const response = await networkRequest(sort, filter, token);
       const data = response.data;
       data.sortOrder = sort;
       dispatch.networkModel.setData(data);
+    },
+    async changeNetworkFilter(filter, rootState) {
+      if (!(filter in rootState.networkModel.feedData)) {
+        await dispatch.networkModel.initFeedDataForKey(filter);
+      }
+      let dataForFilter = rootState.networkModel.feedData[filter];
+      if (
+        dataForFilter &&
+        dataForFilter.data &&
+        dataForFilter.data.length === 0
+      ) {
+        try {
+          dispatch.networkModel.setLoading(true);
+          const response = await networkRequest(
+            rootState.networkModel.sortOrder,
+            filter.toLowerCase(),
+            dataForFilter.token
+          );
+          const data = response.data;
+          dispatch.networkModel.setFeedDataForKey(filter, data);
+        } finally {
+          dispatch.networkModel.setLoading(false);
+        }
+      } else {
+        dispatch.networkModel.setActiveFilter(filter);
+      }
     },
   }),
 };
