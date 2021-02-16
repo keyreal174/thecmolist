305e98fdba1a852943e04ced5d8c004939919580
keyreal174 <keyreal174@gmail.com>
2021-02-16 18:38:16 -0300

    Add engagement buttons functionality to feed

--- src/models/reaction.js
+++ src/models/reaction.js
@@ -58,6 +58,16 @@ const getReactionsForContent = (data) => {
   (data.replies || []).forEach((reply) => {
     reactions[reply.content_id] = getReactions(reply);
   });
+
+  // set reactions data for feed array. Exclude to execute this code in Content
+  if (!data.content_id) {
+    data.forEach((d) => {
+      if (d.content_id) {
+        reactions[d.content_id] = getReactions(d);
+      }
+    });
+  }
+
   return reactions;
 };
 
