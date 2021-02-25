756fbf3911d8d190b34d653cc14a4e4d6c699681
keyreal174 <keyreal174@gmail.com>
2021-02-25 18:27:13 -0300

    Working on onboarding 1 fields

--- src/components/app/Onboarding/OnboardingStep1.js
+++ src/components/app/Onboarding/OnboardingStep1.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import OnboardingLayout from "./OnboardingLayout";
 import { Button, Col, Form, Row } from "react-bootstrap";
+import { useHistory } from "react-router";
 import "./onboardingStep1.scss";
 
 const OnboardingStep1 = () => {
+  const history = useHistory();
+  const handleSubmit = (e) => {
+    console.log("submit");
+    debugger;
+    history.push("onboarding_step2");
+  };
+  const [loading, setLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [companyIndustry, setCompanyIndustry] = useState("");
+  const [companyStage, setCompanyStage] = useState("");
+  const [headline, setHeadline] = useState("");
+  const [networkingYes, setNetworkingYes] = useState("");
+  const [networkingNo, setNetworkingNo] = useState("");
+  const [advidingYes, setAdvidingYes] = useState("");
+  const [advidingNo, setAdvidingNo] = useState("");
+
+  const handleTitleChange = (e) => {
+    const { value } = e.target;
+    setTitle(value);
+  };
+
+  const handleCompanyNameChange = (e) => {
+    const { value } = e.target;
+    setCompanyName(value);
+  };
+
+  const handleCompanyIndustryChange = (e) => {
+    const { value } = e.target;
+    setCompanyIndustry(value);
+  };
+
+  const handleCompanyStageChange = (e) => {
+    const { value } = e.target;
+    setCompanyStage(value);
+  };
+
+  const handleHeadlineChange = (e) => {
+    const { value } = e.target;
+    setHeadline(value);
+  };
+
+  const handleNetworkingYesChange = () => {
+    setNetworkingYes(true);
+    setNetworkingNo(false);
+  };
+
+  const handleNetworkingNoChange = () => {
+    setNetworkingYes(false);
+    setNetworkingNo(true);
+  };
+
+  const handleAdvidingYesChange = () => {};
   return (
     <OnboardingLayout
       now={50}
@@ -12,7 +66,11 @@ const OnboardingStep1 = () => {
       }
     >
       <>
-        <Form className="onboarding--form">
+        <Form
+          className="onboarding--form"
+          id="onboarding_step-1"
+          onSubmit={() => handleSubmit()}
+        >
           <Row>
             <Col md="6">
               <Form.Label>Title</Form.Label>
@@ -20,6 +78,9 @@ const OnboardingStep1 = () => {
                 className="onboarding--input"
                 type="text"
                 required
+                name="title"
+                onChange={handleTitleChange}
+                value={title}
               />
             </Col>
             <Col md="6">
@@ -28,6 +89,9 @@ const OnboardingStep1 = () => {
                 className="onboarding--input"
                 type="text"
                 required
+                name="companyName"
+                onChange={handleCompanyNameChange}
+                value={companyName}
               />
             </Col>
           </Row>
@@ -38,6 +102,9 @@ const OnboardingStep1 = () => {
                 className="onboarding--input"
                 type="text"
                 required
+                name="companyIndustry"
+                onChange={handleCompanyIndustryChange}
+                value={companyIndustry}
               />
             </Col>
             <Col md="6">
@@ -46,6 +113,9 @@ const OnboardingStep1 = () => {
                 className="onboarding--input"
                 type="text"
                 required
+                name="companyStage"
+                onChange={handleCompanyStageChange}
+                value={companyStage}
               />
             </Col>
           </Row>
@@ -57,6 +127,9 @@ const OnboardingStep1 = () => {
                 className="onboarding--text-area"
                 placeholder="E.g., professional marketing leader passionate about consumer Internet, SaaS, and disruptivemarketplaces. Industry expertise: mobile, consumer Internet, social media, enterprise software."
                 rows={3}
+                name="headline"
+                onChange={handleHeadlineChange}
+                value={headline}
               />
             </Col>
           </Row>
@@ -68,11 +141,19 @@ const OnboardingStep1 = () => {
                   className="onboarding--radio"
                   type="radio"
                   label="Yes"
+                  required
+                  name="networking"
+                  onChange={handleNetworkingChange}
+                  value={networking}
                 />
                 <Form.Check
                   className="onboarding--radio"
                   type="radio"
                   label="No"
+                  required
+                  name="networking"
+                  onChange={handleNetworkingChange}
+                  value={networking}
                 />
               </div>
             </Col>
@@ -83,11 +164,19 @@ const OnboardingStep1 = () => {
                   className="onboarding--radio"
                   type="radio"
                   label="Yes"
+                  required
+                  name="adviding"
+                  onChange={handleAdvidingChange}
+                  value={adviding}
                 />
                 <Form.Check
                   className="onboarding--radio"
                   type="radio"
                   label="No"
+                  required
+                  name="adviding"
+                  onChange={handleAdvidingChange}
+                  value={adviding}
                 />
               </div>
             </Col>
@@ -95,7 +184,14 @@ const OnboardingStep1 = () => {
         </Form>
         <Row>
           <Col md="12" className="d-flex justify-content-end">
-            <Button className="onboarding--button">Continue</Button>
+            <Button
+              className="onboarding--button"
+              type="button"
+              form="onboarding_step-1"
+              onClick={(e) => handleSubmit(e)}
+            >
+              Continue
+            </Button>
           </Col>
         </Row>
       </>
