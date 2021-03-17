c38461e54233631a953c04fc38c4128a80de1350
keyreal174 <keyreal174@gmail.com>
2021-03-17 16:30:03 -0300

    Add RichEditor to DiscussionComment

--- src/components/app/base/DiscussionComment/DiscussionComment.js
+++ src/components/app/base/DiscussionComment/DiscussionComment.js
@@ -1,31 +1,50 @@
 import { Button, Form } from "react-bootstrap";
 import React, { useState, useEffect, useRef } from "react";
+import { connect } from "react-redux";
 import clsx from "clsx";
 
 import PersonHeader from "../PersonHeader/PersonHeader";
 
 import "./discussionComment.scss";
+import RichEditor from "../DraftEditor/RichEditor";
 
 const DiscussionComment = ({
-  onSubmit,
   className,
   focusComment,
+  getSuggestions,
+  getTopicSuggestions,
+  onSubmit,
   placeholder,
   profile,
+  useRichEditor,
   value,
   ...props
 }) => {
   const [comment, setComment] = useState(value || "");
   const [show, setShow] = useState("");
+  const [body, setBody] = useState("");
   const textAreaEl = useRef(null);
 
+  const handleButtonClick = (e) => {
+    let text = comment;
+    if (useRichEditor) {
+      text = body;
+    }
+    e.preventDefault();
+    onSubmit(text);
+    setComment("");
+  };
+
   useEffect(() => {
-    if (focusComment) {
+    if (focusComment && !useRichEditor) {
       textAreaEl.current.focus();
       window.scrollTo(0, 0);
     }
   }, []);
 
+  const handleChange = (content) => {
+    setShow(!!body);
+  };
   return (
     <div
       className={clsx(
@@ -41,28 +60,37 @@ const DiscussionComment = ({
           onlyImage={true}
         />
       )}
-      <Form.Control
-        as="textarea"
-        className="comment-input"
-        id="textarea"
-        placeholder={placeholder ? placeholder : "Add a comment..."}
-        onChange={(e) => {
-          const value = e.target.value;
-          setComment(value);
-          setShow(!!value);
-        }}
-        value={comment}
-        ref={textAreaEl}
-        rows={1}
-      />
+      {!useRichEditor && (
+        <Form.Control
+          as="textarea"
+          className="comment-input"
+          id="textarea"
+          placeholder={placeholder ? placeholder : "Add a comment..."}
+          onChange={(e) => {
+            const value = e.target.value;
+            setComment(value);
+            setShow(!!value);
+          }}
+          value={comment}
+          ref={textAreaEl}
+          rows={1}
+        />
+      )}
+      {useRichEditor && (
+        <div className="comment--rich-editor">
+          <RichEditor
+            setBody={setBody}
+            getSuggestions={getSuggestions}
+            getTopicSuggestions={getTopicSuggestions}
+            toolbar={true}
+            handleChange={handleChange}
+          />
+        </div>
+      )}
       {
         <Button
           className={clsx("comment-button", show && "show")}
-          onClick={(e) => {
-            e.preventDefault();
-            onSubmit(comment);
-            setComment("");
-          }}
+          onClick={handleButtonClick}
           variant="primary"
         >
           Post
@@ -72,4 +100,13 @@ const DiscussionComment = ({
   );
 };
 
-export default DiscussionComment;
+const mapStateToProps = () => {};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getSuggestions: dispatch.suggestionsModel.getSuggestions,
+    getTopicSuggestions: dispatch.suggestionsModel.getTopicSuggestions,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DiscussionComment);
