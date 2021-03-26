7614f92fa9bc45a32381f0357ed307abd9caeb02
keyreal174 <keyreal174@gmail.com>
2021-03-26 09:31:33 -0400

    [Task 35] Enable setting placeholder text for the RichEditor

--- src/components/app/base/DraftEditor/DraftEditor.js
+++ src/components/app/base/DraftEditor/DraftEditor.js
@@ -373,6 +373,7 @@ const DraftEditor = ({
             ref={ref}
             handleKeyCommand={handleKeyCommand}
             keyBindingFn={keyBindingFn}
+            placeholder="Include all the information, @people and @vendors someone would need to answer your question"
           />
           {toolbar && (
             <div className="editor-toolbar">
