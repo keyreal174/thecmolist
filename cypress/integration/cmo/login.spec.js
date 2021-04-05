6b68a46203e7173f867e0a1328db4102a952f90e
keyreal174 <keyreal174@gmail.com>
2021-04-05 13:13:53 -0400

    Add cypress test for Feed page

--- cypress/integration/cmo/login.spec.js
+++ cypress/integration/cmo/login.spec.js
@@ -45,7 +45,69 @@ context('Login', () => {
     // submit
     cy.get('.home--form-right')
       .submit()
-      .next()
-      .should('url', '/feed')
+
+    // check redirect url
+    cy.url().should('include', '/feed')
+  })
+
+  // it('Login (linkedin)', () => {
+  //   cy.get('form.home--form-right > div:nth-child(4) > div:nth-child(2) > button')
+  //     .click()
+
+  //   cy.url().should('include', 'https://www.linkedin.com/')
+  // })
+
+  it('Logout', () => {
+    // type email and test
+    cy.get('input[name="email"]')
+      .type('testuser@gmail.com')
+
+    // type password and test
+    cy.get('input[name="password"]')
+      .type('password123')
+
+    // submit
+    cy.get('.home--form-right')
+      .submit()
+
+    // check redirect url
+    cy.url().should('include', '/feed')
+
+    cy.get('#basic-nav-dropdown')
+      .click()
+
+    cy.get('#basic-navbar-nav > div.navbar-nav > div > div > a:nth-child(3)')
+      .click()
+
+    cy.url().should('include', '/')
+    cy.get('#root > div > div > div.home--form.row > div:nth-child(2) > form > div.home--form-subtitle')
+      .should('have.text', 'Already have an account or received a invitation? Sign in here:')
+  })
+
+  it('Logout', () => {
+    // type email and test
+    cy.get('input[name="email"]')
+      .type('test@gmail.com')
+
+    // type password and test
+    cy.get('input[name="password"]')
+      .type('password')
+
+    cy.on('uncaught:exception', (err, runnable) => {
+      console.log(err)
+      expect(err.message).to.include('something about the error')
+  
+      // using mocha's async done callback to finish
+      // this test so we prove that an uncaught exception
+      // was thrown
+      done()
+  
+      // return false to prevent the error from
+      // failing this test
+      return false
+    })
+
+    cy.get('.home--form-right')
+      .submit()
   })
 })
