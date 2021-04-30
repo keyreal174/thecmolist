13b30b0d97cddd0f175f138b689acab3a0929b09
keyreal174 <keyreal174@gmail.com>
2021-04-29 21:22:22 -0700

    small tweak to entities css

--- src/components/app/base/Entities/Entities.js
+++ src/components/app/base/Entities/Entities.js
@@ -8,7 +8,7 @@ const Entities = ({ entities }) => {
 
   return (
     <>
-      <div className="feed-box-content entities-wrapper mt-3">
+      <div className="feed-box-content entities-wrapper">
         {entities
           .filter((item, i) => i < 3 || showMore)
           .map(({ image, name, role, link }, index) => (
