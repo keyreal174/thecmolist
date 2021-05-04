db54fe2196ee3e3decd7e66924ec9007d943ad71
keyreal174 <keyreal174@gmail.com>
2021-05-04 17:00:13 -0300

    Create AllMembersList component

--- src/components/app/Feed/AllMembers.js
+++ src/components/app/Feed/AllMembers.js
@@ -1,21 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import CustomCard from "../base/CustomCard/CustomCard";
 import Avatar from "../base/Avatar/Avatar";
+
+export const AllMembersList = ({ list }) => {
+  return (
+    <>
+      {list &&
+        list.map(({ image, name, role, link }, index) => {
+          return (
+            <a href={link} key={index}>
+              <Avatar image={image} heading={name} subHeading={role} />
+            </a>
+          );
+        })}
+    </>
+  );
+};
 
 const AllMembers = ({ memberList }) => {
   let membersLink = memberList.link || "";
   let list = Array.isArray(memberList) ? memberList : memberList.list;
+
   return list && list.length > 0 ? (
     <CustomCard heading="All Members" seeAllLink={membersLink}>
       <div className="feed-box-content">
-        {list &&
-          list.map(({ image, name, role, link }, index) => {
-            return (
-              <a href={link} key={index}>
-                <Avatar image={image} heading={name} subHeading={role} />
-              </a>
-            );
-          })}
+        <AllMembersList list={list} />
       </div>
     </CustomCard>
   ) : (
