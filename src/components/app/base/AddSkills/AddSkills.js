83cb1e029e95524af5753100a95159c797f3c8d4
keyreal174 <keyreal174@gmail.com>
2021-06-03 15:58:43 -0700

    Add Experience placeholder text Expertise => Experience

--- src/components/app/base/AddSkills/AddSkills.js
+++ src/components/app/base/AddSkills/AddSkills.js
@@ -115,7 +115,7 @@ const RenderSkillCategoryRow = ({
 
   let placeholderText = "";
   if (cate && cate.description && cate.description.length > 0) {
-    placeholderText = `Summarize your ${cate.description} expertise`;
+    placeholderText = `Summarize your ${cate.description} experience`;
   }
   return (
     <Row className="mb-3 align-items-center">
