2e3938669ab7d499668deaba2f01170e54da1917
keyreal174 <keyreal174@gmail.com>
2021-06-03 10:53:57 -0700

    Expertise => Experience

--- src/components/app/base/AddSkills/AddSkills.js
+++ src/components/app/base/AddSkills/AddSkills.js
@@ -113,7 +113,7 @@ const RenderSkillCategoryRow = ({
 
   let placeholderText = "";
   if (cate && cate.description && cate.description.length > 0) {
-    placeholderText = `Summarize your ${cate.description} expertise`;
+    placeholderText = `Summarize your ${cate.description} experience`;
   }
   return (
     <Row className="mb-3 align-items-center">
