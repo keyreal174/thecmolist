0d8738f174c01554f8a00771835a8e37662d9d95
keyreal174 <keyreal174@gmail.com>
2021-06-12 05:23:41 -0400

    Update Post-Onboarding Wizard

--- src/components/app/Feed/BuildYourNetwork.js
+++ src/components/app/Feed/BuildYourNetwork.js
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
 import CustomCard from "../base/CustomCard/CustomCard";
 import CustomCheckBox from "../base/CustomCheckBox/CustomCheckBox";
+import AddVendorsModal from "../base/AddVendors/AddVendorsModal";
+import AddSkillsModal from "../base/AddSkills/AddSkillsModal";
+import { Button } from "react-bootstrap";
+
+const AddVendorButton = ({
+  isVendor,
+  toggleAddVendorModal,
+  toggleAddSkillModal,
+}) => (
+  <Button
+    className="btn-white btn btn-primary post-onboarding-btn"
+    onClick={() => (!isVendor ? toggleAddSkillModal() : toggleAddVendorModal())}
+  >
+    {!isVendor ? "+ Add Expertise" : "+ Add Vendor"}
+  </Button>
+);
 
 const BuildYourNetwork = ({ buildYourNetworkItems }) => {
+  const [showAddVendor, setShowAddVendor] = useState(false);
+  const [showAddSkills, setShowAddSkill] = useState(false);
+
+  const toggleAddVendorModal = () => {
+    setShowAddVendor((value) => !value);
+  };
+
+  const toggleAddSkillModal = () => {
+    setShowAddSkill((value) => !value);
+  };
+
   return (
     <CustomCard heading="Get the most out of using CMOlist">
-      <div>
+      <div className="build-your-network">
         {buildYourNetworkItems &&
           buildYourNetworkItems.map((item, index) => {
             return (
@@ -18,10 +45,22 @@ const BuildYourNetwork = ({ buildYourNetworkItems }) => {
                     if (item.link) window.location.href = item.link;
                   }}
                 />
+                {(index === 1 || index === 2) && (
+                  <AddVendorButton
+                    isVendor={index === 1 ? true : false}
+                    toggleAddVendorModal={toggleAddVendorModal}
+                    toggleAddSkillModal={toggleAddSkillModal}
+                  />
+                )}
               </div>
             );
           })}
       </div>
+      <AddVendorsModal
+        show={showAddVendor}
+        handleClose={toggleAddVendorModal}
+      />
+      <AddSkillsModal show={showAddSkills} handleClose={toggleAddSkillModal} />
     </CustomCard>
   );
 };
