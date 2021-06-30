4bdf0a0d3c78739cbfade64310ddcc36b082e706
keyreal174 <keyreal174@gmail.com>
2021-06-30 11:44:56 -0400

    Change space between image and content in network

--- src/components/app/Vendors/VendorsDetail.js
+++ src/components/app/Vendors/VendorsDetail.js
@@ -1,8 +1,48 @@
 import React from "react";
-import VendorsFeed from "./VendorListFeed";
+import VendorsFeed from "./VendorsFeed";
 
-const VendorsDetail = () => {
-  return <VendorsFeed />;
+const VendorsDetail = (props) => {
+  return (
+    <>
+      {/*<Row>
+        {props.activeFeedSubFilters &&
+          props.activeFeedSubFilters.length > 0 && (
+            <Col className="network--popular-topics" md="4">
+              <PopularTopics
+                onSubfilterChange={(f) => {
+                  props.changeSubFilter(f.slug || f.title);
+                }}
+                topicList={props.activeFeedSubFilters}
+              />
+            </Col>
+          )}
+        <Col
+          className={clsx("network--feed", mobileMenuOpen && "open")}
+          md={
+            props.activeFeedSubFilters &&
+            props.activeFeedSubFilters.length > 0
+              ? "8"
+              : "12"
+          }
+        >
+          {props.loadingNetwork ? (
+            <div className="mt-3 mb-5">
+              <ActivityIndicator className="element-center feed-activity-indicator" />
+            </div>
+          ) : (
+            <NetworkFeed
+              {...props}
+              connectUser={onConnectClick}
+              disconnectUser={disconnectUser}
+              localConnectedUsers={props.localConnectedUsers}
+              fetchData={fetchData}
+              feedData={feedData}
+            />
+          )}
+        </Col>
+          </Row>*/}
+    </>
+  );
 };
 
 export default VendorsDetail;
