9379fea5a80f8cd925cb0d5877391187b8387af7
keyreal174 <keyreal174@gmail.com>
2021-07-22 15:34:55 -0400

    Add carousel for features stacks

--- src/components/app/Feed/FeaturedStacks.js
+++ src/components/app/Feed/FeaturedStacks.js
@@ -1,6 +1,95 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button, Carousel } from "react-bootstrap";
 import CustomCard from "../base/CustomCard/CustomCard";
+import NextIcon from "../../login/icons/arrow-circle-right.svg";
+import PrevIcon from "../../login/icons/arrow-circle-left.svg";
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
+}
+
+const func_chunks = (arr, chunk_num) => {
+  const features = arr.reduce((all, one, i) => {
+    const ch = Math.floor(i / chunk_num);
+    all[ch] = [].concat(all[ch] || [], one);
+    return all;
+  }, []);
+
+  return features || [];
+};
+
+const Slider = ({ featuredStacks }) => {
+  const [features, setFeatures] = useState([]);
+  const [breakpoint, setBreakpoint] = useState(0);
+  const NextButton = <img src={NextIcon} className="" />;
+  const PrevButton = <img src={PrevIcon} className="" />;
+
+  const size = useWindowSize();
+
+  useEffect(() => {
+    if (size.width >= 1400 && breakpoint !== 1) {
+      const temp = func_chunks(featuredStacks, 3);
+      setFeatures(temp);
+      setBreakpoint(1);
+    } else if (size.width < 1400 && size.width >= 1200 && breakpoint !== 2) {
+      const temp = func_chunks(featuredStacks, 2);
+      setFeatures(temp);
+      setBreakpoint(2);
+    } else if (size.width < 1200 && size.width >= 768 && breakpoint !== 3) {
+      const temp = func_chunks(featuredStacks, 1);
+      setFeatures(temp);
+      setBreakpoint(3);
+    }
+  }, [size.width]);
+
+  return (
+    <Carousel
+      nextIcon={NextButton}
+      prevIcon={PrevButton}
+      className="w-100"
+      interval={null}
+      indicators={false}
+    >
+      {features.map((stack, i) => (
+        <Carousel.Item key={i}>
+          <div className="d-flex">
+            {stack.map((feature, index) => (
+              <Feature
+                key={i + "-" + index}
+                image={feature.image}
+                name={feature.name}
+                role={feature.role}
+                link={feature.link}
+              />
+            ))}
+          </div>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
 
 const Feature = ({ image, name, role, link }) => {
   const goViewStack = () => {
@@ -8,7 +97,7 @@ const Feature = ({ image, name, role, link }) => {
   };
 
   return (
-    <div className=" featured-stacks-item ml-2 mr-2">
+    <div className=" featured-stacks-item px-1">
       <CustomCard className="mb-2">
         <div className="user-img">
           <img className="element-center" src={image} alt="avatar" />
@@ -36,16 +125,12 @@ const FeaturedStacks = ({ featuredStacks }) => {
   return featuredStacks && featuredStacks.length > 0 ? (
     <CustomCard heading="Featured marketing stacks">
       <div className="d-flex featured-stacks">
-        {featuredStacks.map((feature, i) => (
-          <Feature
-            key={i}
-            image={feature.image}
-            name={feature.name}
-            role={feature.role}
-            link={feature.link}
-          />
-        ))}
+        <Slider featuredStacks={featuredStacks} />
       </div>
+      <p className="text-center">
+        Browse more trusted vendors <b>shared by your peers</b> in the{" "}
+        <a href="#">vendor directory</a>
+      </p>
     </CustomCard>
   ) : (
     <div />
