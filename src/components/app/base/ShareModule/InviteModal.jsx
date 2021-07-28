7d001df10977fed9f95e2ec4cfded4aef5c8e8b5
keyreal174 <keyreal174@gmail.com>
2021-07-28 06:01:32 -0400

    Build New invite dialog

--- src/components/app/base/ShareModule/InviteModal.jsx
+++ src/components/app/base/ShareModule/InviteModal.jsx
@@ -3,16 +3,61 @@ import { Button, Form, Modal, Row, Col } from "react-bootstrap";
 import Close from "../icons/close.svg";
 import "./sharemodule.scss";
 
+const InfoRow = ({ setUserName, setUserEmail }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  return (
+    <Row>
+      <Col className="invite-modal--input" xs={6}>
+        <Form.Label>Name</Form.Label>
+        <Form.Control
+          className="invite-module-input"
+          type="text"
+          placeholder="First name, Last name"
+          value={name}
+          required={true}
+          onChange={(e) => {
+            setName(e.target.value);
+            setUserName(e.target.value);
+          }}
+        />
+      </Col>
+      <Col className="invite-modal--input" xs={6}>
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          className="invite-module-input"
+          type="email"
+          placeholder="name@company.com"
+          value={email}
+          required={true}
+          onChange={(e) => {
+            setEmail(e.target.value);
+            setUserEmail(e.target.value);
+          }}
+        />
+      </Col>
+    </Row>
+  );
+};
+
 function InviteModal(props) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [name1, setName1] = useState("");
+  const [email1, setEmail1] = useState("");
+  const [name2, setName2] = useState("");
+  const [email2, setEmail2] = useState("");
   const [message, setMessage] = useState("");
   const [collection, setCollection] = useState("");
 
-  let closeDialog = () => {
+  let closeDialog = (e) => {
     let data = {
-      name,
-      email,
+      info: [
+        { name: name, email: email },
+        { name: name1, email: email1 },
+        { name: name2, email: email2 },
+      ],
       message,
       collection,
     };
@@ -51,30 +96,9 @@ function InviteModal(props) {
               <strong>unlock new capabilities</strong>.
             </p>
             <form id="invite-modal" onSubmit={closeDialog}>
-              <Row>
-                <Col className="invite-modal--input" xs={6}>
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control
-                    className="invite-module-input"
-                    type="text"
-                    placeholder="First name, Last name"
-                    value={name}
-                    required={true}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </Col>
-                <Col className="invite-modal--input" xs={6}>
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control
-                    className="invite-module-input"
-                    type="email"
-                    placeholder="name@company.com"
-                    value={email}
-                    required={true}
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </Col>
-              </Row>
+              <InfoRow setUserName={setName} setUserEmail={setEmail} />
+              <InfoRow setUserName={setName1} setUserEmail={setEmail1} />
+              <InfoRow setUserName={setName2} setUserEmail={setEmail2} />
               <Row>
                 <Col xs={12}>
                   <Form.Label>Message</Form.Label>
