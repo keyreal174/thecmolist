2b58e47842d443659bcecef04594624145a3efcd
keyreal174 <keyreal174@gmail.com>
2021-08-02 14:03:31 -0700

    Update for topic selection
    update for topic seleciton

--- src/components/app/Onboarding/OnboardingStep2.js
+++ src/components/app/Onboarding/OnboardingStep2.js
@@ -112,10 +112,10 @@ const OnboardingStep2 = ({
       now={step === 1 ? 75 : 100}
       title={
         showGetIntro
-          ? "One last step..."
+          ? "Finally..."
           : step === 1
           ? "Share five or more of your most impactful marketing tools or agencies with your trusted peers"
-          : "Select three areas of expertise for which you could provide advice to your trusted peers"
+          : "One last step: select topics you would like to learn more about from your peers"
       }
       subtitle={
         showGetIntro
