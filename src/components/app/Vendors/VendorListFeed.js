c00d72a6d3f35057731bf3277104cc493e6cece1
keyreal174 <keyreal174@gmail.com>
2021-09-07 03:51:53 -0400

    Make show all button in Vendors view more prominent

--- src/components/app/Vendors/VendorListFeed.js
+++ src/components/app/Vendors/VendorListFeed.js
@@ -8,39 +8,38 @@ import VendorCard from "./VendorCard";
 import Arrow from "../base/icons/next_arrow.svg";
 import "./vendors.scss";
 
-const VendorListFeed = ({ vendor, filterIdx }) => {
+const SeeAllBtn = ({ vendor, filterIdx }) => {
   const history = useHistory();
-  // FIXME: for the beta we disable pagination as the BE returns all data
-  // POST BETA remove this
+
+  return (
+    <Button
+      className="filter--button-active active m-0 d-flex align-items-center"
+      onClick={() => {
+        history.push({
+          pathname: vendor.link,
+          state: {
+            filterIdx: filterIdx,
+          },
+        });
+      }}
+    >
+      See All
+      <img src={Arrow} alt="Arrow" width="16" height="20" className="ml-2" />
+    </Button>
+  );
+};
+
+const VendorListFeed = ({ vendor, filterIdx }) => {
   return (
     <>
       <div className="vendor-list-divider">
-        <p>Trending Vendors</p>
+        <SeeAllBtn vendor={vendor} filterIdx={filterIdx} />
       </div>
       <div className="vendor-list-container">
         <div className="vendor-list-info">
+          <SeeAllBtn vendor={vendor} filterIdx={filterIdx} />
           <h1>{vendor.name}</h1>
           <p>{vendor.description}</p>
-          <Button
-            className="filter--button filter--button-active active m-0 d-flex align-items-center"
-            onClick={() => {
-              history.push({
-                pathname: vendor.link,
-                state: {
-                  filterIdx: filterIdx,
-                },
-              });
-            }}
-          >
-            See All
-            <img
-              src={Arrow}
-              alt="Arrow"
-              width="16"
-              height="20"
-              className="ml-2"
-            />
-          </Button>
         </div>
         <div className="vendor-list-divider mobile">
           <p>Trending Vendors</p>
