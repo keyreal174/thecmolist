819f3d5ce40bca46ce55b2079170687d4bffd3c4
keyreal174 <keyreal174@gmail.com>
2022-01-18 19:29:17 -0800

    Add new featured members

--- src/components/app/base/FeaturedMembers/FeaturedMembers.js
+++ src/components/app/base/FeaturedMembers/FeaturedMembers.js
@@ -32,6 +32,60 @@ const MEMBERS = [
     role: "Head of Marketing at Nextdoor",
     link: "vendor/oracle",
   },
+  {
+    image:
+      "https://d3k6hg21rt7gsh.cloudfront.net/1632750850829michael-marcellin.jpg",
+    name: "Michael Marcellin",
+    role: "CMO at Juniper Networks",
+    link: "vendor/oracle",
+  },
+  {
+    image:
+      "https://d3k6hg21rt7gsh.cloudfront.net/1617649682876ashley-stirrup.jpg",
+    name: "Ashley Stirrup",
+    role: "Former CMO Algolia",
+    link: "vendor/oracle",
+  },
+  {
+    image:
+      "https://d3k6hg21rt7gsh.cloudfront.net/eyJidWNrZXQiOiJjbW9saXN0aW1hZ2VzIiwia2V5IjoiMTY0MjA0MzU0NDQ2OWEuanBlZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MjAwLCJoZWlnaHQiOjIwMCwiZml0IjoiY292ZXIifX19",
+    name: "Christy Huggins",
+    role: "Head of Marketing at OneSignal",
+    link: "vendor/oracle",
+  },
+  {
+    image: "https://d3k6hg21rt7gsh.cloudfront.net/1633386860591helen-min.jpg",
+    name: "Helen Min",
+    role: "Head of Marketing at AngelList",
+    link: "vendor/oracle",
+  },
+  {
+    image:
+      "https://d3k6hg21rt7gsh.cloudfront.net/1629773188024robin-zucker.jpg",
+    name: "Robin Zucker",
+    role: "CMO at Codecademy",
+    link: "vendor/oracle",
+  },
+  {
+    image:
+      "https://d3k6hg21rt7gsh.cloudfront.net/1634084347866stacey-politi.jpg",
+    name: "Stacey Politi",
+    role: "CMO at MainStreet",
+    link: "vendor/oracle",
+  },
+  {
+    image:
+      "https://d3k6hg21rt7gsh.cloudfront.net/1619825894001darren-goode.jpg",
+    name: "Darren Goode",
+    role: "CMO at Zumper",
+    link: "vendor/oracle",
+  },
+  {
+    image: "https://d3k6hg21rt7gsh.cloudfront.net/1632796872473jason-lyman.jpg",
+    name: "Jason Lyman",
+    role: "SVP Marketing at BetterCloud",
+    link: "vendor/oracle",
+  },
 ];
 
 const Feature = ({ image, name, role, link, color }) => {
